cda99460000cbd19869a6c6533f72dcfafa8d9f4
David <david.campion@walmartlabs.com>
2022-12-03 13:00:47 -0800

    add everything

--- src/core.rs
+++ src/core.rs
@@ -11,5 +11,5 @@ pub fn get_data(day: &str) -> std::io::Result<String> {
 }
 
 pub fn get_lines(day: &str) -> std::io::Result<Vec<String>> {
-    Ok(get_data(&day)?.lines().map(String::from).collect())
+    Ok(get_data(day)?.lines().map(String::from).collect())
 }
