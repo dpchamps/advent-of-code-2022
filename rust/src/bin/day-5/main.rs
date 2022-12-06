6d72d5c197626b370d519228b9f0dc5132d41ef2
David <david.campion@walmartlabs.com>
2022-12-05 21:40:15 -0800

    complete solutions

--- rust/src/bin/day-5/main.rs
+++ rust/src/bin/day-5/main.rs
@@ -1,45 +1,22 @@
+use std::fmt::{Display, Formatter};
+use advent_of_code_2022::core::{get_data, get_lines};
 
+#[derive(Eq, PartialEq, Debug)]
+enum Value {
+    Int(usize),
+}
+#[derive(Eq, PartialEq, Debug)]
+struct MoveInstruction(Value, Value, Value);
 
-struct StateParser {}
-
-impl StateParser {
-    fn parse_line(line: &String) -> Result<Vec<char>, String> {
-        line.chars()
-            .collect::<Vec<char>>()
-            .chunks(4)
-            .map(|register| match register.get(1) {
-                Some(value) => Ok(*value),
-                None => Err(format!(
-                    "Expected value in register, but got nothing. Input line: {}",
-                    line
-                )),
-            })
-            .collect()
-    }
-
-    pub fn create_vm_registers(input: &[String]) -> Result<Vec<Vec<char>>, String> {
-        input
-            .iter()
-            .take(input.len() - 1)
-            .map(StateParser::parse_line)
-            .collect::<Result<Vec<Vec<char>>, String>>()
-    }
+#[derive(Eq, PartialEq, Debug)]
+enum Instruction {
+    MoveInstruction(MoveInstruction),
 }
 
 #[derive(Eq, PartialEq, Debug)]
-enum Command {
-    Move(usize),
-    From(usize),
-    To(usize),
-}
-
-#[derive(Eq, PartialEq, Debug)]
-struct Instruction(Box<Program>, Box<Program>, Box<Program>);
-
-#[derive(Eq, PartialEq, Debug)]
-enum Program {
+enum ProgramNode {
     Instruction(Instruction),
-    Command(Command),
+    Command(Value),
 }
 
 struct ProgramParser {
@@ -110,10 +87,10 @@ impl ProgramParser {
             .map_err(|_| format!("Failed to parse digit {}", digit_buffer))
     }
 
-    fn parse_digit_directive(&mut self, command: &str) -> Result<usize, String> {
+    fn parse_int_value(&mut self, var_name: &str) -> Result<Value, String> {
         self.skip_whitespace();
 
-        for c in command.chars() {
+        for c in var_name.chars() {
             self.eat(c).ok_or(format!(
                 "Expected {} at {}, but found {:?}",
                 c,
@@ -122,42 +99,21 @@ impl ProgramParser {
             ));
         }
 
-        self.parse_digit()
+        Ok(Value::Int(self.parse_digit()?))
     }
 
-    fn parse_move(&mut self) -> Result<Program, String> {
-        println!("parsing next move");
-        Ok(Program::Command(Command::Move(
-            self.parse_digit_directive("move")?,
+    fn parse_instruction(&mut self) -> Result<ProgramNode, String> {
+        Ok(ProgramNode::Instruction(Instruction::MoveInstruction(
+            MoveInstruction(
+                self.parse_int_value("move")?,
+                self.parse_int_value("from")?,
+                self.parse_int_value("to")?),
         )))
     }
 
-    fn parse_from(&mut self) -> Result<Program, String> {
-        println!("parsing next from");
-        Ok(Program::Command(Command::From(
-            self.parse_digit_directive("from")?,
-        )))
-    }
-
-    fn parse_to(&mut self) -> Result<Program, String> {
-        println!("parsing next to");
-        Ok(Program::Command(Command::To(
-            self.parse_digit_directive("to")?,
-        )))
-    }
-
-    fn parse_instruction(&mut self) -> Result<Program, String> {
-        Ok(Program::Instruction(Instruction(
-            Box::new(self.parse_move()?),
-            Box::new(self.parse_from()?),
-            Box::new(self.parse_to()?),
-        )))
-    }
-
-    pub fn parse_program(&mut self) -> Result<Vec<Program>, String> {
-        let mut program: Vec<Program> = vec![];
+    pub fn parse_program(&mut self) -> Result<Vec<ProgramNode>, String> {
+        let mut program: Vec<ProgramNode> = vec![];
         while self.current().is_some() {
-            println!("parsing next instruction");
             program.push(self.parse_instruction()?)
         }
 
@@ -166,17 +122,160 @@ impl ProgramParser {
 }
 
 struct Vm {
-    registers: Option<Vec<Vec<char>>>,
-    program: Option<Vec<Program>>,
+    registers: Vec<Vec<char>>,
+}
+
+impl Display for Vm {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let max_register_len = self.registers.iter().map(|x| x.len()).max().expect("");
+        let mut registers = String::new();
+        let register_list = (1..=self.registers.len()).map(|x| format!(" {}  ", x)).collect::<String>();
+        for i in 0..=max_register_len {
+            let i = max_register_len-i;
+            for register in self.registers.iter() {
+                match register.get(i){
+                    Some(content) => registers.push_str(&format!("[{}] ", content)),
+                    _ => registers.push_str("    ")
+                }
+            }
+            registers.push('\n');
+        }
+
+        write!(
+            f,
+            "VM State -----\n{}{}\n------------",
+            registers,
+            register_list
+        )
+    }
+}
+
+impl Vm {
+    fn exec_move(&mut self, m: usize, f: usize, t: usize) -> Result<(), String> {
+        let from_length = self.registers[f].len();
+        let drain_range = from_length-m..from_length;
+        let mut to_append: Vec<char> = self.registers[f].drain(drain_range).collect();
+        self.registers[t].append(&mut to_append);
+
+        Ok(())
+    }
+
+    fn execute_instruction(&mut self, instruction: &Instruction) -> Result<(), String> {
+        match instruction {
+            Instruction::MoveInstruction(MoveInstruction(
+                                             Value::Int(m),
+                                             Value::Int(f),
+                                             Value::Int(t),
+            )) => self.exec_move(*m, f - 1, t - 1),
+            _ => Err(format!("Got an unknown instruction {:?}", instruction)),
+        }
+    }
+
+    pub fn run_program(&mut self, program: &[ProgramNode]) -> Result<(), String> {
+        for node in program {
+            match node {
+                ProgramNode::Instruction(instruction) => self.execute_instruction(instruction),
+                _ => return Err(format!("Encountered an invalid ProgramNode: {:?}", node)),
+            };
+        }
+
+        Ok(())
+    }
+
+    pub fn get_tops_of_stacks(&self) -> String {
+        self.registers.iter().map(|reg| match reg.last() {
+            Some(c) => c.to_string(),
+            None => "".to_string()
+        })
+            .collect()
+    }
+}
+
+struct StateParser {}
+
+impl StateParser {
+    fn get_max_line_len(lines: &[String]) -> usize {
+        lines.iter().map(|x| x.len()).max().expect("Got something unexpected for max_line_len")
+    }
+
+    fn parse_line(line: &String) -> Result<Vec<(usize, Option<char>)>, String> {
+        Ok(line.chars()
+            .collect::<Vec<char>>()
+            .chunks(4)
+            .map(|register| match register.get(1) {
+                Some(value) if *value == ' ' => Ok(None),
+                Some(value) => Ok(Some(*value)),
+                None => Err(format!(
+                    "Expected value in register, but got nothing. Input line: {}",
+                    line
+                )),
+            })
+            .collect::<Result<Vec<Option<char>>, String>>()?
+            .into_iter()
+            .enumerate()
+            .collect())
+    }
+
+    pub fn create_vm_registers(input: &[String]) -> Result<Vec<Vec<char>>, String> {
+        let n_registers = (StateParser::get_max_line_len(input)+1) / 4;
+        let mut registers = vec![Vec::new(); n_registers];
+
+        for line in input.iter().take(input.len()-1) {
+            for (register, maybe_val) in StateParser::parse_line(line)? {
+                match maybe_val {
+                    Some(c) => registers[register].insert(0, c),
+                    None => ()
+                }
+            }
+        }
+
+        Ok(registers)
+    }
+}
+
+fn parse_input(input: &str) -> Result<(Vm, Vec<ProgramNode>), String> {
+    let split = input.split("\n\n").map(String::from).collect::<Vec<String>>();
+    let registers_string = split.get(0).ok_or("Unexpected, couldn't find registers!")?;
+    let mut program_parser = ProgramParser::new(split.get(1).ok_or("Unexpected, couldn't find program!")?);
+
+    return Ok((
+        Vm {
+            registers: StateParser::create_vm_registers(
+                &registers_string.lines().map(String::from).collect::<Vec<String>>(),
+            )?,
+        },
+        program_parser.parse_program()?,
+    ));
+}
+
+fn part_one(input: &String) -> Result<String, String> {
+    let (mut vm, program) = parse_input(&input)?;
+
+    vm.run_program(&program);
+
+    Ok(vm.get_tops_of_stacks())
 }
 
 fn main() -> std::io::Result<()> {
+    let input = get_data("day-5")?;
+
+    println!("Part One: {}", part_one(&input).expect("Could not solve Part One"));
     Ok(())
 }
 
 #[cfg(test)]
 mod day_5_tests {
     use crate::*;
+
+    const INPUT: &str = "    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
 
     #[test]
     fn state_parser_smoke_test() {
@@ -190,8 +289,8 @@ mod day_5_tests {
 
         let registers = StateParser::create_vm_registers(&input).expect("Could not create VM");
 
-        assert_eq!(registers[0].len(), 1);
-        assert_eq!(registers[0][0], 'D');
+        assert_eq!(registers[0].len(), 3);
+        assert_eq!(registers[0][0], 'Z');
         assert_eq!(registers.len(), 3);
     }
 
@@ -207,22 +306,64 @@ move 2 from 2 to 1";
         assert_eq!(
             &program,
             &[
-                Program::Instruction(Instruction(
-                    Box::new(Program::Command(Command::Move(1))),
-                    Box::new(Program::Command(Command::From(2))),
-                    Box::new(Program::Command(Command::To(1)))
-                )),
-                Program::Instruction(Instruction(
-                    Box::new(Program::Command(Command::Move(3))),
-                    Box::new(Program::Command(Command::From(1))),
-                    Box::new(Program::Command(Command::To(3)))
-                )),
-                Program::Instruction(Instruction(
-                    Box::new(Program::Command(Command::Move(2))),
-                    Box::new(Program::Command(Command::From(2))),
-                    Box::new(Program::Command(Command::To(1)))
-                ))
+                ProgramNode::Instruction(Instruction::MoveInstruction(MoveInstruction(
+                    Value::Int(1),
+                    Value::Int(2),
+                    Value::Int(1),
+                ))),
+                ProgramNode::Instruction(Instruction::MoveInstruction(MoveInstruction(
+                    Value::Int(3),
+                    Value::Int(1),
+                    Value::Int(3),
+                ))),
+                ProgramNode::Instruction(Instruction::MoveInstruction(MoveInstruction(
+                    Value::Int(2),
+                    Value::Int(2),
+                    Value::Int(1),
+                ))),
             ]
         )
+    }
+
+    #[test]
+    fn vm_smoketest() {
+        let initial_state = "[D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3 "
+            .lines()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+        let mut vm = Vm {
+            registers: StateParser::create_vm_registers(&initial_state)
+                .expect("Could not create VM Registers"),
+        };
+
+        let instruction = Instruction::MoveInstruction(MoveInstruction(
+            Value::Int(1),
+            Value::Int(1),
+            Value::Int(2),
+        ));
+
+
+        vm.execute_instruction(&instruction)
+            .expect("Could not execute instruction");
+
+    }
+
+    // #[test]
+    // fn part_one_example(){
+    //     let (mut vm, program) = parse_input(INPUT).expect("Could not create input");
+    //     vm.run_program(&program).expect("Failed to run program");
+    //
+    //     assert_eq!(vm.get_tops_of_stacks(), "CMZ");
+    // }
+
+    #[test]
+    fn part_two_example(){
+        let (mut vm, program) = parse_input(INPUT).expect("Could not create input");
+        vm.run_program(&program).expect("Failed to run program");
+
+        assert_eq!(vm.get_tops_of_stacks(), "MCD");
     }
 }
